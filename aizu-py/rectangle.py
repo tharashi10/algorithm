ffb829d8a447ea47fc43788945c7d0473a29a4b1
takahiro.harashima <takhiro.harashima@oracle.com>
2022-08-24 09:24:17 +0000

    refactoring

--- aizu-py/rectangle.py
+++
@@ -1,4 +0,0 @@
-x,y = map(int, input().split())
-s = x*y
-peri = (x+y)*2
-print(s,peri)
--- aizu-rust/dir_cargo/target/debug/deps/dir_cargo-7939ef451730a707.d
+++
@@ -1,5 +0,0 @@
-/home/opc/algorithm/sandbox/dir_cargo/target/debug/deps/dir_cargo-7939ef451730a707: src/main.rs
-
-/home/opc/algorithm/sandbox/dir_cargo/target/debug/deps/dir_cargo-7939ef451730a707.d: src/main.rs
-
-src/main.rs:
