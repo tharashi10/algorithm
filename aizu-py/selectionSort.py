a0f9c2535e7151b019107b39d9fe393e4209edd2
takahiro.harashima <takhiro.harashima@oracle.com>
2022-08-25 15:11:30 +0000

    selectionSort

--- aizu-py/selectionSort.py
+++ aizu-py/selectionSort.py
@@ -2,15 +2,15 @@ n = int(input())
 A = list(map(int,input().split()))
 
 def selectionSort(n,A):
-    for i in range(0,n)):
+    cnt = 0
+    for i in range(0,n):
         min_idx = i
-        cnt = 0
-        for j in range(i,n)):
+        for j in range(i,n):
             if A[min_idx] > A[j]:
                 min_idx=j
-                A[min_idx],A[i]=A[i],A[min_idx]
-                cnt+=1
-            print("index: %s",i)
+        A[min_idx],A[i]=A[i],A[min_idx]
+        if A[i] < A[min_idx]:
+            cnt+=1
     print(*A)
     print(cnt)
 selectionSort(n,A)
