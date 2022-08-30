369bf04e7b9f483ddeb56c5113d55e824411b1c6
takahiro.harashima <takhiro.harashima@oracle.com>
2022-08-31 01:51:36 +0900

    rooted Tree modified

--- aizu-py/rootedTree.py
+++ aizu-py/rootedTree.py
@@ -2,7 +2,14 @@
 根っこのNode(節点)を特別扱いする
 ポイント:親子兄弟を表す構造体を用意する
 状態遷移図（左子右兄弟関係）
+--
+print(sys.getrecursionlimit())
+1000
 '''
+import sys
+sys.setrecursionlimit(3*15)
+
+# 構造体
 # p:親, l:左Child, r:右Brother
 # rとlは同じ深さにいない
 class Node:
@@ -12,55 +19,81 @@ class Node:
         self.r = r
 
 # 末端から根っこに向かって親がいるか探索していく
-def get_depth(T,v):
+def get_depth(T,vid):
     '''
     T:Tree
     v:Node
     '''
     d = 0
-    while T[v].p is not None:
-        v = T[v].p
+    # 親がいる限りはひたすらループ
+    while T[vid].p is not None:
+        vid = T[vid].p #親ノードに移る
         d +=1
 
 Depth = {}
 # 根っこからスタートし、左→右の流れで検索していく
 # v:節点のID
-# p:現在いる深さ
-def get_all_depth(Tree,v,p):
-    Depth[v] = p
-    if Tree[v].r is not None:
-        get_all_depth(Tree, Tree[v].r, p)
-    if Tree[v].l is not None:
-        get_all_depth(Tree, Tree[v].l, p+1)
+# q:現在いる深さ
+# 深さのList Depth[]を生成する
+def get_all_depth(Tree,vid,q):
+    Depth[vid] = q
+    # 兄弟いるなら横に移動
+    if Tree[vid].r is not None:
+        get_all_depth(Tree, Tree[vid].r, q)
+    
+    # 左下の子いるならに移動(深さIncrement)
+    if Tree[vid].l is not None:
+        get_all_depth(Tree, Tree[vid].l, q+1)
 
-# ある節点(ノード)に対する子を返す
-def return_children(Tree,v):
+# ある節点(ノード)に対する子たちを返す
+def return_children(Tree,vid):
     children = []
-    while(Tree[v].l is not None):
-        children.append(Tree[v].l)
-        c = Tree[v].r
+    c = Tree[vid].l
+    while(c is not None):
+        children.append(c)
+        c = Tree[c].r #右がなくなるまで探索してる
     return children
 
 
 n = int(input())
-# lambdaで
+
+# リスト内包表記でDict作成
 Tree = {id: Node(None,None,None) for id in range(n)}
+# 標準入力値を整形する temp→Treeを作成
 for _ in range(n):
     tmp = list(map(int,input().split()))
-    if tmp[1] == 0: #次数がZero
-        continue
-    Tree[tmp[0]].l = tmp[2]
-    Tree[tmp[2]].p = tmp[0]
-    tmp_sibling = tmp[2]
+    if tmp[1] == 0: #　次数KがZeroの時
+        continue # これ以降は処理しない
+    vid = tmp[0]
+    cl = tmp[2]
+    Tree[vid].l = cl #親からみた子
+    Tree[cl].p = vid #子からみた親
+    tmp_sibling = tmp[2] #v1,v2...と見ていくがv1は既知なので保存しておく
     for sib in tmp[3:]:
         Tree[tmp_sibling].r = sib
-        Tree[sib].p = tmp[0]
+        Tree[sib].p = vid
         tmp_sibling = sib
 
-for node in range(n):
-    if Tree[node].p is None:
-        get_all_depth(Tree, node, 0) #開始点はRootのみ
+#print([Tree[i] for i in range[n]])
+
+# 深さDepthリストを作る
+for vid in range(n):
+    if Tree[vid].p is None:
+        get_all_depth(Tree, vid, 0) #開始点はRootのみ
         break
-#
-#for node in range(n):
-#    node_type = 
+
+# 1ノードに対するPrint関数(出力形式に合わせる)
+def print_single_node(Tree,vid,Depth):
+    node_type = 'internal node' if Tree[vid].l is not None else 'leaf'
+    if Tree[vid].p is None:
+        parent = -1
+        node_type = 'root'
+    else:
+        parent = Tree[vid].p
+    children = return_children(Tree,vid)
+    print(
+        f'node {vid}: parent = {parent}, depth = {Depth[vid]}, {node_type}, {children}'
+    )
+
+for vid in range(n):
+    print_single_node(Tree,vid,Depth)
