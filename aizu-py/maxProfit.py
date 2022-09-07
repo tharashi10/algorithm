a45e65d80309ef0a07a55cfe170e1f9cdbae1a10
tharashi10 <takahiro.harashima@oracle.com>
2022-09-08 00:43:33 +0900

    maxProfit.py

--- aizu-py/maxProfit.py
+++ aizu-py/maxProfit.py
@@ -1,9 +1,14 @@
-N = int(input())
-A = [int(input()) for i in range(N)]
-
 def getMaxProfit(A):
+    # minv: Aのこれまで出てきた数値の最小値
+    # maxv: 得られる最大利益
     minv = A[0]
+    maxv = A[1]-A[0]
     for j in range(1,len(A)):
-        maxv = 1 if A[j] < A[j+1] else 2
-        minv = 1 if A[j] < A[j+1] else 2
-print(A)
+        maxv =  maxv if A[j]-minv < maxv else A[j]-minv
+        minv =  A[j] if minv > A[j] else minv
+    print(maxv)
+
+N = int(input())
+l = [int(input()) for i in range(N)]
+
+getMaxProfit(l)
