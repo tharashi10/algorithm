d54a3e35ec8f85cedd73fbd0929b06218981cb94
takahiro.harashima@oracle.com <takahiro.harashima@oracle.com>
2022-09-19 12:12:27 +0900

    dictionary

--- aizu-py/01.search/dictionary.py
+++ aizu-py/01.search/dictionary.py
@@ -1,13 +1,25 @@
-N = int(input())
-commands = []
-for _ in range(N):
-    command = list(input().split())
-    commands.append(command)
+"""
+Input()
+6
+insert AAA
+insert AAC
+find AAA
+find CCC
+insert CCC
+find CCC
+--------
+Output()
+yes
+no
+yes
+"""
 
-def search(dict, x):
-    exists=False
-    for j in range(len(dict)):
-        if dict[j]==x:
+# findの場合Searchする
+def search(dic, st):
+    exists = False
+    for j in range(len(dic)):
+        #print(dic)
+        if dic[j]==st:
             exists=True
             break
         else:
@@ -17,9 +29,25 @@ def search(dict, x):
     else:
         print('no')
 
-def execute(N,X):
+def execute(n, X):
     my_dict = []
-    for i in range(N):
-        {my_dict.append(X[i][1]) if X[i][0]=="insert" else search(my_dict,X[i][1])}
+    for i in range(n):
+        {my_dict.append(X[i][1]) if X[i][0]=="insert" else search(my_dict, X[i][1])}
 
-execute(N,commands)
+if __name__ == "__main__":
+    """: n=999997とかでやるとTEとなる（listが遅いため→Setで）
+    n = int(input())
+    commands = []
+    for _ in range(n):
+        command = list(input().split())
+        commands.append(command)
+    execute(n, commands)
+    """
+    d = set()
+    for _ in range(int(input())):
+        command, s = input().split()
+        if command == "find":
+            print("yes" if s in d else "no")
+        else:
+            d.add(s)
+    #print(d)
