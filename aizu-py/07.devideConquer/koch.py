501d9af92383aca878d20bf11aa9a0525c1382b4
takahiro.harashima@oracle.com <takahiro.harashima@oracle.com>
2022-09-23 21:23:05 +0900

    koch curve

--- aizu-py/07.devideConquer/koch.py
+++ aizu-py/07.devideConquer/koch.py
@@ -19,6 +19,8 @@ def koch(n,p1,p2):
     s = Point((2*p1_x + p2_x)/3,(2*p1_y + p2_y)/3)
     t = Point((p1_x + 2*p2_x)/3,(p1_y + 2*p2_y)/3)
 
+    # ここの式を間違えないよう気を付ける
+    # import cmathで回転座標変換も可能
     ux = (t.x-s.x)*math.cos(math.radians(60)) - (t.y-s.y)*math.sin(math.radians(60))+s.x
     uy = (t.x-s.x)*math.sin(math.radians(60)) + (t.y-s.y)*math.cos(math.radians(60))+s.y
     u = Point(ux,uy)
