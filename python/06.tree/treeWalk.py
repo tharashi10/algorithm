d7fd88528171a44f816848001e91ec18c76892f7
takahiro.harashima@oracle.com <takahiro.harashima@oracle.com>
2022-09-27 23:56:47 +0900

    treeWalk

---
+++ python/06.tree/treeWalk.py
@@ -0,0 +1,26 @@
+'''Binary Tree Walk
+In
+9
+0 1 4
+1 2 3
+2 -1 -1
+3 -1 -1
+4 5 8
+5 6 7
+6 -1 -1
+7 -1 -1
+8 -1 -1
+-------
+Out
+Preorder
+ 0 1 2 3 4 5 6 7 8
+Inorder
+ 2 1 3 0 6 5 7 4 8
+Postorder
+ 2 3 1 6 7 5 8 4 0
+'''
+
+def preorder():
+def interorder():
+def postoder():
+
