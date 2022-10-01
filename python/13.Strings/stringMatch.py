4cb04f17fb68f93e6c9281a9429c309a2c79e361
takahiro.harashima@oracle.com <takahiro.harashima@oracle.com>
2022-10-01 16:46:43 +0900

    stringMatch

---
+++ python/13.Strings/stringMatch.py
@@ -0,0 +1,21 @@
+# ALDS1_14_A
+"""
+Input: 
+aabaaa
+cc
+Output:
+0 
+3 
+4
+"""
+for i in range(2):
+    if i ==0:
+       str1 = input()
+    else:
+        str2 = input() 
+
+cnt = 0
+for i in range(0,len(str1)-len(str2)+1):
+    if str1[i:i+len(str2)] == str2:
+        print(i)
+        cnt +=1
