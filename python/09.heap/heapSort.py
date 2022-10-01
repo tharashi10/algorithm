bb5fc20dbf5b97d37e205353244f3d10e073f9e4
takahiro.harashima@oracle.com <takahiro.harashima@oracle.com>
2022-10-01 16:18:38 +0900

    heapSort_WorstSwapCase

---
+++ python/09.heap/heapSort.py
@@ -0,0 +1,55 @@
+''': TODO: 要検討
+ヒープソート
+Maxheapifyを利用する
+Input
+10
+4 1 3 2 16 9 10 14 8 7
+--------
+Output
+16 14 10 8 7 9 3 2 4 1
+'''
+
+def maxHeapify(A,i,n):
+    # 再帰に入った瞬間に、Leftが存在することに留意する(図書くとわかる)
+    # Nを2で割ってるので、Rightがあるかないかの2パターンがあるはず
+    left = 2*i
+    right = 2*i+1
+    
+    if left > n+1:
+        return
+
+    if A[left]>A[i]:
+        largest = left
+    else:
+        largest = i
+
+    if right <=n:
+        if A[right]>A[largest]:
+            largest= right
+    
+    if largest!=i:
+        A[i],A[largest] = A[largest],A[i]
+        maxHeapify(A,largest,n)
+
+def buildHeap(A,n): 
+    for i in reversed(range(1,n//2+1)):
+        maxHeapify(A,i,n)
+    return A
+
+if __name__ == "__main__":
+    n = int(input())
+    A0 = ["None"]
+    A1 = list(map(int,input().split()))
+    
+    A = A0+A1
+    maxOrdered = buildHeap(A,n)
+    l = maxOrdered[1:]
+    heapsize = n
+
+    # TODO Swap回数のWorstCaseを算出する
+    while heapsize>=2:
+        A[1], A[heapsize] = A[heapsize], A[1]
+        heapsize-=1
+        maxHeapify(A[0:heapsize],1,heapsize)
+    print(l)
+    #print(' '+' '.join(map(str,l))) #ここの先頭空白部分は意外と盲点(Tipsへ追記)
