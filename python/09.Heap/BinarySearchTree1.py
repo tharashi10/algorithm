5e48b10ebdb6b364355cb2a40aecccdf08d97da0
takahiro.harashima@oracle.com <takahiro.harashima@oracle.com>
2022-10-03 00:49:19 +0900

    BinaryTreeSearch

--- python/09.Heap/BinarySearchTree1.py
+++ python/09.Heap/BinarySearchTree1.py
@@ -17,40 +17,70 @@ print
 """"
 
 class Node:
-    def __init__(self,parent,right,left):
+    def __init__(self,key,parent,right,left):
+        self.key = key
         self.parent = parent
         self.right = right
         self.left = left
 
-def insert(Tree, z):
-    y = None
-    x = Tree[0]
-    while x != None:
+def insert(Tree, num):
+    y = None # y: numと比較するやつ
+    x = Tree[0] #root
+
+    # rootがあればぐるぐる
+    while x != Node(-1,-1,-1,-1):
         y = x
-        if z.key < y.key:
+        if num.key < y.key:
             x = x.left
         else:
             x = x.right
-    z.parent = y
+    
+    num.parent = y # num : Node(key,parent,right,left)
+                   # ex  : Node(23,y,-1,-1)  
 
-    # Treeが空
-    if y == None:
+    # Treeが空かどうかで分岐
+    if y == Node(-1,-1,-1,-1):
         Tree[0] = z
-    elif z.key < y.key:
-        y.left = z
+    elif num.key < y.key:
+        y.left = num
     else:
-        y.right = z
+        y.right = num
 
 
 def print():
+    return None
+
+
+def preorder(vid):
+    print(f' {vid}', end="")
+    if node[vid].left == -1 and node[vid].right == -1:
+        return
+    if node[vid].left != -1:
+        preorder(node[vid].left)
+    if node[vid].right != -1:
+        preorder(node[vid].right)   
+
+def interorder(vid):
+    if vid == None:
+        return
+    if node[vid].left != -1:
+        interorder(node[vid].left)
+    print(f' {vid}',end="")
+    if node[vid].right != -1:
+        interorder(node[vid].right)
 
 
 if __name__ == "__main__":
     n = input()
-    T = []
+    node = []
+
+    # init
+    for i in range(n):
+        node.append(Node(-1,-1,-1))
+    
     for i in range(n):
         s = str(input())
         if s.startwith("insert"):
-            insert(T, s[7:])
+            insert(node, s[7:])
         else:
             print()
