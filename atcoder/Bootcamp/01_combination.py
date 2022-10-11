955e2ea27c21697408e6e7ff80d419f921ad2ee9
takahiro.harashima@oracle.com <takahiro.harashima@oracle.com>
2022-10-11 12:35:41 +0900

    01

--- atcoder/Bootcamp/01_combination.py
+++ atcoder/Bootcamp/01_combination.py
@@ -1,14 +1,34 @@
 """:01
-重複無しで３つの数を選びそれらの合計が x となる組み合わせの数
+重複無しで「３つ」の数を選びそれらの合計が x となる組み合わせの数
+In: 5 9
+    0 0
+Out: 2
+実行速度:01.20 s
 """
 
 while True:
-    n,s = input().split()
+    n,s = map(int,input().split())
     if n==0:
         break
     # 全探索する
+    # i < j < k とする
+    cnt = 0
     lst =[i for i in range(1,n+1)]
+    
     for i in lst:
-        for j in range(n-1):
-            for k in range(n-2):
-for i in range()
+        tmp1 = s
+        lst1 = list(filter(lambda x : x>i, lst))
+        tmp1-=i
+        
+        for j in lst1:
+            tmp2 = tmp1
+            lst2 = list(filter(lambda x : x>j, lst1))
+            tmp2-=j
+
+            for k in lst2:
+                tmp3 = tmp2
+                tmp3-=k
+                if tmp3 ==0:
+                    cnt+=1
+                    #print(i,j,k)
+    print(cnt)
