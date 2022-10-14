534e755f507c6f60267ea258f5e997f7650774a5
takahiro.harashima@oracle.com <takahiro.harashima@oracle.com>
2022-10-14 19:07:39 +0900

    07

--- atcoder/Bootcamp/07_ArchaelogicalSites.py
+++ atcoder/Bootcamp/07_ArchaelogicalSites.py
@@ -1,4 +1,5 @@
-"""
+""":最古の遺跡
+日本情報オリンピック本選考
 10
 9 4
 4 3
@@ -17,21 +18,27 @@ point_list = []
 square = 0
 
 for i in range(n):
-    point_list.append(list(map(int,input().split())))
+    point_list.append(tuple(map(int,input().split())))
 
+# in zip()は使うな.くそ遅い.TLEにならない
+# setで、removeは使うな. 引数Checkのハンドリングめんどい
 for i in range(n-1):
     for j in range(i+1,n):
-        p1 = [point_list[i][0],point_list[i][1]]
-        p2 = [point_list[j][0],point_list[j][1]]
+        ll = set(point_list)
+        p1 = (point_list[i][0],point_list[i][1])
+        p2 = (point_list[j][0],point_list[j][1])
+        vec = (p2[0]-p1[0],p2[1]-p1[1]) 
+        vertical = (-vec[1],vec[0])
         
-        vec = [y-x for x,y in zip(p1,p2)]
-        vec_vertical = [-vec[1],vec[0]]
-        p1_ = [x+y for x,y in zip(vec_vertical, p1)]
-        p2_ = [x+y for x,y in zip(vec_vertical, p2)]
-        
-        ll = list(filter(lambda x: x not in [p1,p2], point_list))
-        if p1_ in ll and p2_ in ll:
-            tmp = sum([i**2 for i in vec])
+        #ll = list(filter(lambda x: x not in [p1,p2], point_list))
+        ll.discard(p1)
+        ll.discard(p2) # setでは discard 使える
+        p1_ = (p1[0]+vertical[0],p1[1]+vertical[1])
+        p2_ = (p2[0]+vertical[0],p2[1]+vertical[1])
+
+        if (p1_) in ll and (p2_) in ll:
+            tmp = vec[0]*vec[0] + vec[1]*vec[1]
             if square < tmp:
                 square=tmp
+
 print(square)
