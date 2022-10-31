a0579c167398d3b5789f5428e910b2f42cc246d1
takahiro.harashima@oracle.com <takahiro.harashima@oracle.com>
2022-10-31 20:59:03 +0900

    25

--- atcoder/Bootcamp/25_Islands.py
+++ atcoder/Bootcamp/25_Islands.py
@@ -1,8 +1,12 @@
 """:
 How many islands?
+わかった
+→ アルゴリズムノート(2)のP4に図を記載
+
 方針
-・再帰関数版の深さ優先探索で実装してみる
+・再帰関数版の深さ優先探索で実装する
 ・Inputがやや癖があるので、各々のケースに対してDFSを適用させる
+・一回到達した陸は、重複しないように海にしちゃう
 
 https://atcoder.jp/contests/atc001/tasks/dfs_a
 Input
@@ -18,31 +22,51 @@ Output
 
 A = []
 h,w = map(int,input().split())
-Already = [[-1]*w]*h
-time = 0
+reached = [[False]*w]*h
+cnt = 0
 
 for i in range(h):
     A.append(list(map(int,input().split())))
 
 def dfs(xid,yid):
-    global time
-    if xid<0 or xid>h or yid<0 or yid>=w:
-        return
+    global cnt
     
+    print(f"Start Checking: xid,yid={xid},{yid}")
+    if xid<0 or xid>=h or yid<0 or yid>=w:
+        return
+
+    if reached[xid][yid]:
+        return
+
+    reached[xid][yid] = True
     if A[xid][yid]==0:
-        #print("0000")
-        return 
-    
-    if Already[xid][yid] != -1:
         return
+    A[xid][yid] = 0
+    
+    dfs(xid,yid-1)
+    dfs(xid,yid)
+    dfs(xid,yid+1)
 
-    Already[xid][yid] = time
-    time+=1
+    dfs(xid+1,yid-1)
+    dfs(xid+1,yid)
+    dfs(xid+1,yid+1)
 
-    dfs(xid+1,yid  )
-    dfs(xid-1,yid  )
-    dfs(xid  ,yid+1)
-    dfs(xid  ,yid-1)
+    dfs(xid-1,yid-1)
+    dfs(xid-1,yid)
+    dfs(xid-1,yid+1)
 
-dfs(0,0)
-print(Already)
+for i in range(len(A)):
+    for j in range(len(A[i])):
+        print(i,j)
+        if (i,j)==(0,0):
+            print("IF")
+            dfs(i,j)
+            cnt+=1
+        else:
+            print(f"ELSE: {i}, {j}")
+            print(f"A:{A}")
+            if not reached and A[i][j]==1:
+                dfs(i,j)
+                cnt+=1
+
+print(cnt)
