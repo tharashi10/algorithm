556818f21851ad1c41cc7a0efa0fdc6892851026
takahiro.harashima@oracle.com <takahiro.harashima@oracle.com>
2022-11-02 15:44:28 +0900

    28

--- atcoder/Bootcamp/28_AOJ_BFS.py
+++ atcoder/Bootcamp/28_AOJ_BFS.py
@@ -1,5 +1,11 @@
 """
-BFS:
+BFS
+方針: BFS(x)で値を探索しにいく
+https://qiita.com/drken/items/996d80bcae64649a6580
+ポイント:
+・いつ距離distを入れるか
+・発見済みのものはAppendしない(ここ、と書いたとこ)
+
 4
 1 2 2 4
 2 1 4
@@ -11,3 +17,28 @@ BFS:
 3 2
 4 1
 """
+n = int(input())
+A = [[]]
+dist = [0,0]+[-1 for _ in range(n-1)]
+que = [1]
+
+for i in range(n):
+    tmp = list(map(int,input().split()))
+    k = tmp[1]
+    ll = [tmp[j+2] for j in range(k)]
+    A.append(ll)
+
+def bfs(x):
+    while len(que)!=0:
+        vid = que.pop(0)
+        if vid==x:
+            break
+        for v in A[vid]:
+            if dist[v]!=-1: #←ここ
+                continue
+            dist[v]=(dist[vid]+1)
+            que.append(v)
+
+bfs(100) #　100は適当な値
+for i in range(1,n+1):
+    print(f"{i} {dist[i]}")
