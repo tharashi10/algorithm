0676650787fdee9cf6f6141e29c63de88357e064
takahiro.harashima@oracle.com <takahiro.harashima@oracle.com>
2022-11-10 02:11:48 +0900

    42

--- atcoder/Bootcamp/42_Silkload.py
+++ atcoder/Bootcamp/42_Silkload.py
@@ -1,8 +1,17 @@
 """
 方針:
-・m日目に、都市iにいた場合の最小疲労度をdp[i][m]とおく。
+・m日目に、都市iにいる場合の最小疲労度をdp[i][m]とおく。
 ・求めたい最小疲労度は、dp[N][1],dp[N][2],.....dp[N][M]の最も小さい疲労度となる。
-→ dp[N][M]
+→ dpを手書きで書いた(45分くらい...)
+
+・書くとわかるが、dp表の対角線で切った左半分は無限大となる。
+なぜなら、例えば都市3にDay1で辿り着くことは、本問題の場合不可能であるから。
+よって、iの値によってmの範囲が固定される。
+
+・また、初期条件としては、dp[0]=[0,0,0,0,..0]となる。ここは無限ではない。
+
+・また、dp の無限大を 10**6 にすると、Codeはあってても1個もACされない。
+→ 無限を入れたい場合は、10**10 とする癖をつけておくこと！！！
 
 3 5
 10
@@ -17,26 +26,25 @@
 1125
 """
 N, M = map(int,input().split())
-DIST = []
+D = []
 C = []
 
 for i in range(N):
-    DIST.append(int(input()))
+    D.append(int(input()))
 for j in range(M):
     C.append(int(input()))
 
+# dpは　行(N+1)×列(M+1) となる
 dp = [[10**6]*(M+1) for i in range(N+1)]
-dp[0][0] = 0
+for i in range(M+1):
+    dp[0][i] = 0
 
 for i in range(1,N+1):
-    #dp[i][m] = dp[i][m-1] # 待機する場合
-    #dp[i][m] = dp[i-1][m-1] + CLIM[m-1]*DIST[i-1] # 進める場合
-    for m in range(i,M-N+i+1):
-        dp[i][m] = min(dp[i][m-1],dp[i-1][m-1]+C[m-2]*DIST[i-2])
-        
-#for i in range(N+1):
-#    print(*dp[i])
-#print(C)
-print(min(dp[N]))
-#dp[1][] = dp[0][] + c[0]*d[0]
+    # Case1) dp[i][m] = dp[i][m-1] # 待機する場合
+    # Case2) dp[i][m] = dp[i-1][m-1] + C[m-1]*D[i-1] # 進める場合
+    for m in range(i,M+1):
+        dp[i][m] = min(dp[i][m-1],dp[i-1][m-1]+C[m-1]*D[i-1])
 
+#for i in range(N+1):
+#    print(dp[i])
+print(f"{min(dp[N])}")
