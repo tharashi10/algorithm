b197e19c8d00850a96b1439caa5388bbe87255f0
takahiro.harashima@oracle.com <takahiro.harashima@oracle.com>
2022-11-10 21:13:35 +0900

    43

--- atcoder/Bootcamp/43_Hata.py
+++ atcoder/Bootcamp/43_Hata.py
@@ -1,5 +1,12 @@
 """
 パ研軍旗
+・やはりdp書けば解ける.
+・「こいつ」と書いたところ、10*10で計算してACせず1時間くらいはまった...前回も同じ感じでやってしまったのに
+---dp
+10000000000 4 8 10
+10000000000 4 8 11
+10000000000 4 7 12
+
 --Input
 3
 WWR
@@ -15,7 +22,7 @@ N = int(input())
 A = []
 for i in range(5):
     st = str(input())
-    A.append([0] + [ st[j] for j in range(N)])
+    A.append([0] + [st[j] for j in range(N)])
 
 dp = []
 # 行は3色
@@ -39,14 +46,16 @@ dp[2][1] = 5-tmp_ini.count(clist[2])
 for i in range(2,N+1):
     tmp_i = []
     for flg in range(5):
-        tmp_i.append(A[flg][i])
+        tmp_i.append(A[flg][i]) # tmp_i = ['R','#','#','B','W']
     
-    for j in range(3):
-        for jj in range(3):
-            if j!=jj:
-                dp[j][i] = min(dp[j][i],dp[jj][i-1]+(5-tmp_i.count(clist[j])))
+    for key in range(3):
+        cdict = {0:'R',1:'B',2:'W'}
+        pp = cdict.pop(key) # ex. key=0の時、cdict={1:'B',2:'W'},pp=R
+        jj = [int(k) for k,v in cdict.items()]
+        
+        dp[key][i] = min(dp[jj[0]][i-1],dp[jj[1]][i-1])+(5-tmp_i.count(pp))
 
-ans = 10*10
+ans = 10**10 #こいつ
 for i in range(3):
     ans = min(ans,dp[i][N])
 
