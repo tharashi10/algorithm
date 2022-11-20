1f3ef56485a7412e88ab4f95a3eff19c5500d706
takahiro.harashima@oracle.com <takahiro.harashima@oracle.com>
2022-11-20 13:04:05 +0900

    54

--- atcoder/Bootcamp/54_TrumpInsert.py
+++ atcoder/Bootcamp/54_TrumpInsert.py
@@ -1,8 +1,10 @@
 """
 トランプ挿入ソート
+
 https://www.slideshare.net/chokudai/abc006
 上記のスライドP45からのGIFで、LISをわかりやすく説明している
 
+#53と同じ解き方
 6
 1
 3
@@ -13,3 +15,21 @@ https://www.slideshare.net/chokudai/abc006
 --
 2
 """
+import bisect
+
+def main():
+    N = int(input())
+    seq = [int(input()) for _ in range(N)]
+
+    LIS = [seq[0]]
+    for i in range(N):
+        if seq[i] > LIS[-1]:
+            LIS.append(seq[i])
+        else:
+            idx = bisect.bisect_left(LIS,seq[i])
+            LIS[idx] = seq[i]
+    
+    print(N-len(LIS))
+
+if __name__=="__main__":
+    main()
