a3052158ea1b9ef8985eec7f3624af20fd87056c
takahiro.harashima@oracle.com <takahiro.harashima@oracle.com>
2022-12-04 19:42:06 +0900

    77

---
+++ atcoder/Bootcamp/77_Tabibito.py
@@ -0,0 +1,40 @@
+"""
+旅人
+累積和
+これは余裕
+
+7 5
+2
+1
+1
+3
+2
+1
+2
+-1
+3
+2
+-3
+ans=18
+"""
+
+from itertools import accumulate
+
+def main():
+    N,M = map(int,input().split())
+    l = [0,0]+[int(input()) for i in range(N-1)]
+    t = [int(input()) for i in range(M)]
+    acc = list(accumulate(l))
+    MOD = int(1e5)
+
+    ans = 0
+    now = 1
+    for i in range(M):
+        next = now + t[i] # 3
+        ans+=abs(acc[next]-acc[now])%MOD
+        now = next
+    
+    print(ans%MOD)
+
+if __name__=="__main__":
+    main()
