dfcdf79fd716f3d19bf1ce1daff5129964e33744
takahiro.harashima@oracle.com <takahiro.harashima@oracle.com>
2022-12-16 18:39:13 +0900

    85

--- atcoder/Bootcamp/85_UnionFind.py
+++ atcoder/Bootcamp/85_UnionFind.py
@@ -2,6 +2,10 @@
 Union Find
 このスライドが最もわかりやすい
 https://atcoder.jp/contests/atc001/tasks/unionfind_a
+
+set.recutsionlimit()を入れないと、
+テストケース21.でRuntime Errorになる。(結構な数をテストケースに入れてある模様)
+---
 5 12
 0 1 4
 0 2 3
@@ -25,38 +29,49 @@ https://atcoder.jp/contests/atc001/tasks/unionfind_a
 1
 1
 """
+import sys
+sys.setrecursionlimit(int(1e7))
 
 def main():
     N,M = map(int,input().split())
     par = [i for i in range(N)]
     
-    def find(x): # 自分の親を見つける
+    def root(x): # 自分の親を見つける
         if par[x]==x:
             return x
         else:
-            par[x]=find(par[x]) # 経路圧縮
+            par[x]=root(par[x]) # 経路圧縮
             return par[x]
     
     def same(x,y): # 親が一緒か判定する
-        return find(x)==find(y)
+        rx = root(x)
+        ry = root(y)
+        return rx == ry
 
     def unite(x,y): # それぞれの親を確認して、異なるなら親をマージする
-        x = find(x)
-        y = find(y)
-        if x==y:
-            return 0
-        par[x]=y
+        rx = root(x)
+        ry = root(y)
+        if rx==ry:
+            return None
+        par[rx]=ry
     
+    query = []
+
     for _ in range(M):
-        c,x,y = map(int,input().split())
-        if c==0:
+        query.append(list(map(int,input().split())))
+    
+    for i in range(M):
+        q = query[i][0]
+        x = query[i][1]
+        y = query[i][2]
+        if q==0:
             unite(x,y)
             continue
-        elif c==1:
+        elif q==1:
             if same(x,y):
-                print("1")
+                print('1')
             else:
-                print("0")
+                print('0')
 
 if __name__=="__main__":
     main()
