68c45bac025034272b8a577409d95f6e400b3e96
takahiro.harashima@oracle.com <takahiro.harashima@oracle.com>
2022-12-20 12:07:42 +0900

    88

---
+++ atcoder/Bootcamp/88_Goishi.py
@@ -0,0 +1,28 @@
+"""
+碁石ならべ
+ランレングス圧縮する(連長圧縮)
+
+0=White
+1=Black
+8
+1
+0
+1
+1
+0
+0
+0
+0
+
+https://developer.mozilla.org/en-US/docs/Learn/Tools_and_testing/Client-side_JavaScript_frameworks/React_accessibility
+https://spring.io/guides/tutorials/react-and-spring-data-rest/
+https://atug.tokyo/?p=490
+https://cstack.github.io/db_tutorial/parts/part5.html
+https://www.youtube.com/watch?v=oLO7kLNJt7A
+"""
+
+def main():
+
+
+if __name__=="__main__":
+    main()
