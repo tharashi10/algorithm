014a0603d1eb01491ea773f3d1fd64a59a64202a
takahiro.harashima@oracle.com <takahiro.harashima@oracle.com>
2022-12-23 01:35:13 +0900

    88

--- atcoder/Bootcamp/88_Goishi.py
+++ atcoder/Bootcamp/88_Goishi.py
@@ -3,6 +3,15 @@
 ランレングス圧縮する(連長圧縮)
 decode/encodeの実装はPythonでList(tuple(char,int))使って簡単にできる
 
+めちゃめちゃ粘ってACできた。時間かかった。
+時間がかかった部分は、listのRemoveのところ。末尾を消す意図で
+list.remove(len(list),(color,int))のようにしていたけど、
+どうしてもListの順序が途中で変わってしまって、最後のElse文のところで2個前が正しく参照できていなかった。
+→そのため、removeではなく、pop()で消すようにしたところ上手くいった。
+→remove/insertのように、indexを指定して消す/追加する時は気をつけたほうがいい。
+
+あと、RunLengthのメソッドを今回は使ってないが、備忘として残しておく
+
 0=White
 1=Black
 -------
@@ -43,29 +52,30 @@ def main():
     ll.append((str(input()),1))
     for i in range(N-1):
         color = str(input())
-        atLast = ll[len(ll)-1]
+        atLast = ll[-1]
 
         # Same Color
-        if atLast[0] == color:
-            ll.remove(atLast)
-            ll.insert(len(ll),(color,atLast[1]+1))
+        if atLast[0] == color: # 4th/6th/7th/8th
+            ll.pop()
+            ll.append((color,atLast[1]+1))
 
         # Different Color
+        elif i%2==1: # Odd 3rd/5th
+            ll.append((color,1)) #奇数ならイロチでも塗り替えはしない
+            
+        elif len(ll)==1: # 2nd
+            # イロチかつ偶数
+            # 新規に置く碁石の色で塗り替える
+            ll.pop()
+            ll.append((color,atLast[1]+1))
+            
         else:
-            if i%2==1: # Odd
-                ll.append((color,1)) #奇数ならイロチでも塗り替えはしない
-              
-            else:
-                # 新規に置く碁石の色で塗り替える
-                if len(ll)==1:
-                    ll.remove(atLast)
-                    ll.insert(len(ll),(color,atLast[1]+1))
-                else:
-                    atSecondLast = ll[len(ll)-2]
-                    ll.remove(atLast)
-                    ll.remove(atSecondLast)
-                    ll.insert(len(ll),(color,atLast[1]+atSecondLast[1]+1))
-        #print(ll)
+            atSecondLast = ll[-2]
+            ll.pop()
+            ll.pop()
+            ll.append((atSecondLast[0],atLast[1]+atSecondLast[1]+1))
+            
+        #print(f"*** {i+2}th : ll={ll} ***")
     ans = 0
     for i in range(len(ll)):
         if ll[i][0]==str(0):
