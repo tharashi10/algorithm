06d7f94376bfd5ab09809c16163d06f77a2f6575
takahiro.harashima@oracle.com <takahiro.harashima@oracle.com>
2022-12-23 23:59:45 +0900

    edu

---
+++ c++/educational/01_Youkan.cpp
@@ -0,0 +1,19 @@
+/** 01
+https://github.com/E869120/kyopro_educational_90/blob/main/problem/001.jpg
+https://github.com/E869120/kyopro_educational_90/blob/main/sample/001.txt
+7 45
+2
+7 11 16 20 28 34 38
+--
+12
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int N,L;
+    cin >> N >> L;
+    cout << N<< L <<endl;
+    return 0;
+}
