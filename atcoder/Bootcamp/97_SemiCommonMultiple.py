72caff1c0eaf34d23136c4f7637c77fa51cdcee0
takahiro.harashima@oracle.com <takahiro.harashima@oracle.com>
2022-12-28 19:52:14 +0900

    97

--- atcoder/Bootcamp/97_SemiCommonMultiple.py
+++ atcoder/Bootcamp/97_SemiCommonMultiple.py
@@ -1,4 +1,6 @@
 """
+ロジックあってる
+PythonだとREする
 2 50
 6 10
 """
@@ -11,20 +13,10 @@ def main():
     
     def lcm(nums):
         return functools.reduce(math.lcm,nums)
-    
+
     ll = list(map(lambda x: x//2, ll))
-    mi = lcm(ll)
-    ans = 0
-    
-    tmp = 0
-    while (tmp<M):
-
-    for k in range(1,n,2):
-        tmp = k*mi
-        if :
-            break
-        ans+=1
-
+    com = lcm(ll)
+    ans = (M//com+1)//2 # Mまでに最小公倍数が何個あるのかを算出(但し奇数を算出)
     print(ans)
 
 if __name__ == "__main__":
